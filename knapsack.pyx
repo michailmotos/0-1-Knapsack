5a9dbd2be2844f607332b6c750974c438996aeee
michailmotos <michailmotos@gmail.com>
2019-03-05 19:25:14 +0200

    Update knapsack.pyx

--- knapsack.pyx
+++ knapsack.pyx
@@ -2,23 +2,20 @@ import numpy as np
 cimport numpy as np
 import math
 
-#ctypedef np.float_t DTYPE_t
 
 
-def solve_cython(int capacity,int items,weights,values):
-    #cdef np.ndarray[long, ndim=2] grid = np.empty((items + 1, capacity + 1), dtype=long)
-    grid = [[float(0) for i in range(capacity+1)] for w in range(items+1)]
-    grid = np.array(grid).astype(float)
+def solve_cython(int capacity,int itemSize,weightDist,utilityDist):
+    result = [[float(0) for i in range(capacity+1)] for w in range(itemSize+1)]
+    result = np.array(result).astype(float)
 
-    for item in range(items+1):
-        this_weight = math.floor(weights[item])
-        this_value = values[item]
-        #grid[item + 1, :this_weight] = grid[item, :this_weight]
+    for item in range(itemSize+1):
+        currWeight = math.floor(weights[item])
+        currValue = utilityDist[item]
 
         for k in range(0, capacity + 1):
-            if weights[item] <= k:
-                grid[item][k] = max(grid[item-1][k], grid[item-1][k - this_weight] + this_value)
+            if weightDist[item] <= k:
+                result[item][k] = max(result[item-1][k], result[item-1][k - currWeight] + currValue)
             else:
-                grid[item][k] = grid[item-1][k]
+                result[item][k] = result[item-1][k]
     #print(grid)
-    return grid[items, capacity]
+    return result[itemSize, capacity]
